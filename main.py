3481403a64bd5fd6b393cf48d883956ff754f145
Isaac <isaac.cho.us@gmail.com>
2023-04-10 22:33:51 -0700

    First Commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import time
+import threading
+import tkinter as tk
+from tkinter import ttk, PhotoImage
+
+class studyTimer:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("600x300")
+        self.root.title("Study Time!")
+        self.root.tk.call('wm', 'iconphoto', self.root._w, PhotoImage(file = image.png))
+
+    def start_timer_thread(self):
+        pass
+
+    def start_timer(self):
+        pass
+
+    def reset_clock(self):
+        pass
+
+    def skip_clock(self):
+        pass
+
+
+
+
+
+# https://www.youtube.com/watch?v=FJeXp5yZd-g
